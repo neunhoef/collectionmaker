e8f90d54c98225f93a26b408b32bdc95c0dc356b
Max Neunhoeffer <max@arangodb.com>
2020-10-08 12:04:56 +0200

    Changes from old repo.

--- smart-graph-maker.go
+++ smart-graph-maker.go
@@ -219,18 +219,17 @@ func runRandomTest(db driver.Database, runTimeSeconds int,
 	startTime := time.Now()
 	limit := time.Duration(runTimeSeconds) * time.Second
 	for time.Now().Sub(startTime) < limit {
-		// Run another 10000 random access queries:
-		times := make([]time.Duration, 0, 10000)
-		for i := 1; i <= 10000; i++ {
+		// Run another 1000 random access queries:
+		times := make([]time.Duration, 0, 1000)
+		for i := 1; i <= 1000; i++ {
 			start := time.Now()
 			startVertex := fmt.Sprintf(
 				"instances/ten%d:K%d",
 				firstTenantNr+rand.Intn(lastTenantNr+1-firstTenantNr),
-				rand.Intn(pathsPerTenant))
+				rand.Intn(pathsPerTenant)+1)
 			query := fmt.Sprintf(
 				`FOR v, e IN 2..2 OUTBOUND "%s" GRAPH "G" RETURN v`,
 				startVertex)
-			fmt.Printf("Query: %s\n", query)
 			cursor, err := db.Query(nil, query, nil)
 			if err != nil {
 				fmt.Printf("Error running query: %v\n", err)
@@ -249,16 +248,16 @@ func runRandomTest(db driver.Database, runTimeSeconds int,
 			cursor.Close()
 			times = append(times, time.Now().Sub(start))
 			if count != 1 {
-				fmt.Printf("Got wrong count: %d\n", cursor.Count())
-				return fmt.Errorf("Banana")
+					fmt.Printf("Got wrong count: %d, key: %s, query: %s\n", count, startVertex, query)
 			}
+			cursor.Close()
 		}
 		sort.Sort(DurationSlice(times))
 		var sum int64 = 0
 		for _, t := range times {
 			sum = sum + int64(t)
 		}
-		fmt.Printf("Times for last 10000: %s (median), %s (90%%ile), %s (99%%ilie), %s (average)", times[5000], times[9000], times[9900], time.Duration(sum/10000))
+		fmt.Printf("Times for last 1000: %s (median), %s (90%%ile), %s (99%%ilie), %s (average)\n", times[500], times[900], times[990], time.Duration(sum/1000))
 	}
 	return nil
 }
